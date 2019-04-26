4f3ef26297d9dd4342122c83c2fab76a76c91901
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2019-04-26 16:15:25 +0200

    Changed docker build option order

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 version="$(sh version.sh)"
-docker build . --build-arg quarkusVersion=${version} -t quarkus-jvm:${version}
+docker build --build-arg quarkusVersion=${version} -t quarkus-jvm:${version} .
