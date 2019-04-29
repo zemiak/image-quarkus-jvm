14699ff3659f22112e50650af38777cf1f4ed5ae
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2019-04-29 17:20:02 +0200

    Added schedulet

--- generate-libs.sh
+++ generate-libs.sh
@@ -5,7 +5,7 @@ quarkusVersion="$(sh version.sh)"
 rm -rf temp
 rm -rf lib/*
 mvn io.quarkus:quarkus-maven-plugin:${quarkusVersion}:create -DprojectGroupId=temp -DprojectArtifactId=temp \
-    -Dextensions="smallrye-health, smallrye-metrics, smallrye-openapi, smallrye-fault-tolerance, smallrye-jwt, resteasy, resteasy-jsonb, arc, smallrye-rest-client, hibernate-validator, hibernate-orm, jdbc-postgresql" || exit 10
+    -Dextensions="smallrye-health, smallrye-metrics, smallrye-openapi, smallrye-fault-tolerance, smallrye-jwt, resteasy, resteasy-jsonb, arc, smallrye-rest-client, hibernate-validator, hibernate-orm, jdbc-postgresql, scheduler" || exit 10
 cd temp
 mvn package || exit 20
 cp target/lib/* ../lib/
